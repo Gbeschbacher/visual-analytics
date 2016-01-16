0e810aed7aa9c78a3e4c8c202855c5b54cd928d0
Lukas Wanko <lwanko.mmt-m2014@fh-salzburg.ac.at>
2016-01-16 09:22:04 +0100

    feature: added awesomeness

--- data/mysql/reader.js
+++ data/mysql/reader.js
@@ -19,7 +19,26 @@ connectionpool = mysql.createPool({
 
 
 
-app.get('/:measurment', function (req, res) {
+app.get('/', function (req, res) {
+
+    // Results in average hmw per parameter per month = 7 parameters * 12 months = 84 rows
+    if(req.params.resolution === "m"){
+        query = 'SELECT SUM(hmw)/COUNT(*), parameter, MONTHNAME(datetime) FROM Measurements GROUP BY parameter, MONTH(datetime)';
+    }
+    // Results in average hmw per parameter per week = 7 parameters * 52 months = 364 rows
+    // Submit week?
+    else if(req.params.resolution === "w"){
+        query = 'SELECT SUM(hmw)/COUNT(*), parameter, WEEK(datetime) FROM Measurements GROUP BY parameter, WEEK(datetime)';
+    }
+    // Results in average hmw per parameter per quarter = 7 parameters * 4 quarters = 28 rows
+    else if(req.params.resolution === "q"){
+        query = 'SELECT SUM(hmw)/COUNT(*), parameter, QUARTER(datetime) FROM Measurements GROUP BY parameter, QUARTER(datetime)';
+    }
+    // Results in average hmw per parameter per location per quarter = 4 * 7 * 12 = 336
+    else if(req.params.location === true){
+        query = 'SELECT SUM(hmw)/COUNT(*), location, parameter, QUARTER(datetime) FROM Measurements GROUP BY parameter, QUARTER(datetime), location';
+    }
+
     connectionpool.getConnection(function (err, connection) {
         if (err) {
             console.error('CONNECTION error: ', err);
@@ -29,7 +48,7 @@ app.get('/:measurment', function (req, res) {
                 err: err.code
             });
         } else {
-            connection.query('SELECT * FROM measurments ORDER BY id DESC', function (err, rows, fields) {
+            connection.query(query, function (err, rows, fields) {
                 if (err) {
                     console.error(err);
                     res.statusCode = 500;
